7a94c118658528f5097cba50ca0ee8a9f43fdf57
aygalic <aygalic@hotmail.fr>
2025-01-14 17:42:42 +0100

    chore: playing around by adding a couple more examples

--- llm2vec/demo.py
+++ llm2vec/demo.py
@@ -16,7 +16,8 @@ model = AutoModel.from_pretrained(
     trust_remote_code=True,
     config=config,
     torch_dtype=torch.bfloat16,
-    device_map="cuda" if torch.cuda.is_available() else "cpu",
+    #    device_map="cuda" if torch.cuda.is_available() else "cpu",
+    device_map="cpu",
 )
 model = PeftModel.from_pretrained(
     model,
@@ -39,6 +40,7 @@ instruction = (
 queries = [
     [instruction, "how much protein should a female eat"],
     [instruction, "summit define"],
+    [instruction, "What is a cat"],
 ]
 q_reps = l2v.encode(queries)
 
@@ -46,12 +48,14 @@ q_reps = l2v.encode(queries)
 documents = [
     "As a general guideline, the CDC's average requirement of protein for women ages 19 to 70 is 46 grams per day. But, as you can see from this chart, you'll need to increase that if you're expecting or training for a marathon. Check out the chart below to see how much protein you should be eating each day.",
     "Definition of summit for English Language Learners. : 1  the highest point of a mountain : the top of a mountain. : 2  the highest level. : 3  a meeting or series of meetings between the leaders of two or more governments.",
+    "A cat is feline",
+    "A cat is just like a dog"
 ]
 d_reps = l2v.encode(documents)
 
 # Compute cosine similarity
-q_reps_norm = torch.nn.functional.normalize(q_reps, p=2, dim=1)
-d_reps_norm = torch.nn.functional.normalize(d_reps, p=2, dim=1)
+q_reps_norm = torch.nn.functional.normalize(q_reps, p=len(queries), dim=1)
+d_reps_norm = torch.nn.functional.normalize(d_reps, p=len(documents), dim=1)
 cos_sim = torch.mm(q_reps_norm, d_reps_norm.transpose(0, 1))
 
 print(cos_sim)
