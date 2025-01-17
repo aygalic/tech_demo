fed60c56e21980bf1143b3d82e2c548873ecd914
aygalic <aygalic@hotmail.fr>
2025-01-17 10:33:57 +0100

    chore: moving ressources to proper json file

--- llm2vec/model_comparison.py
+++ llm2vec/model_comparison.py
@@ -1,18 +1,19 @@
+import json
+
 import torch
 from peft import PeftModel
+from sentence_transformers import SentenceTransformer
 from transformers import AutoConfig, AutoModel, AutoModelForCausalLM, AutoTokenizer
 
 from llm2vec import LLM2Vec
 
-from ressources import queries, poetry_documents, science_documents
-import json
-
-from sentence_transformers import SentenceTransformer
-
 ORIGINAL_MODEL_ID = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 LLM2VEC_MODEL_ID = "McGill-NLP/LLM2Vec-Meta-Llama-31-8B-Instruct-mntp"
 LLM2VEC_LORA_MODEL_ID = "McGill-NLP/LLM2Vec-Meta-Llama-31-8B-Instruct-mntp-supervised"
-SBERT_MODEL_ID = "sentence-transformers/all-mpnet-base-v2"  # One of the best performing SBERT models
+SBERT_MODEL_ID = (
+    "sentence-transformers/all-mpnet-base-v2"  # One of the best performing SBERT models
+)
+
 
 class CustomLLM2Vec(LLM2Vec):
     """Custom LLM2Vec class that handles CausalLM outputs"""
@@ -66,14 +67,45 @@ def load_llm2vec_model():
     model = PeftModel.from_pretrained(model, LLM2VEC_LORA_MODEL_ID)
     return model, tokenizer
 
+
 def load_sbert_model():
     """Load SentenceBERT model"""
     return SentenceTransformer(SBERT_MODEL_ID)
+
 
 def prepare_sbert_input(queries):
     """Prepare input for SBERT (removes instruction prefix)"""
     # SBERT doesn't use instructions, so we'll just use the queries
     return [q[1] if isinstance(q, list) else q for q in queries]
+
+
+def get_data(
+    json_path: str = "assets/llm2vec/ressources.json",
+) -> tuple[list[str], list[str], list[str]]:
+    """Retrieve experiment data from json file
+
+    Parameters
+    ----------
+    json_path : str, optional
+        Path of the json file to load data from,
+        by default 'assets/llm2vec/ressources.json'
+
+    Returns
+    -------
+    tuple[list[str], list[str], list[str]]
+        Tuple of list containing queries, poetry documents and science documents
+    """
+
+    with open(json_path, encoding="utf-8") as f:
+        ressources = json.load(f)
+
+    instruct = ressources["instruction"]
+    queries = [instruct + query for query in ressources["queries"]]
+    poetry_documents = ressources["documents"]["poetry_documents"]
+    science_documents = ressources["documents"]["science_documents"]
+    breakpoint()
+
+    return queries, poetry_documents, science_documents
 
 
 def compare_models():
@@ -91,19 +123,22 @@ def compare_models():
     )
 
     # Prepare documents
+
+    queries, poetry_documents, science_documents = get_data()
+
     selected_docs = poetry_documents[:1] + science_documents[:1]
     selected_docs = poetry_documents + science_documents
 
     print("Computing embeddings...")
-    
+
     # Original model embeddings
     orig_q_reps = original_wrapper.encode(queries)
     orig_d_reps = original_wrapper.encode(selected_docs)
-    
+
     # LLM2Vec model embeddings
     l2v_q_reps = llm2vec_wrapper.encode(queries)
     l2v_d_reps = llm2vec_wrapper.encode(selected_docs)
-    
+
     # SBERT embeddings
     sbert_queries = prepare_sbert_input(queries)
     sbert_q_reps = torch.tensor(sbert_model.encode(sbert_queries))
@@ -118,8 +153,9 @@ def compare_models():
         "SBERT_documents": sbert_d_reps.cpu().numpy().tolist(),
     }
 
-    with open('output.json', 'w+', encoding='utf8') as json_file:
+    with open("output.json", "w+", encoding="utf8") as json_file:
         json.dump(results, json_file)
+
 
 if __name__ == "__main__":
     compare_models()
